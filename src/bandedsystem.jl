78316f559a027b1f7779bea481585fa917db9c0e
sandreza <andrenogueirasouza@gmail.com>
2022-03-30 23:37:35 -0400

    add single direction to banded system

--- src/bandedsystem.jl
+++ src/bandedsystem.jl
@@ -23,3 +23,30 @@ function batchedbandedlu(
     mat = Bennu.batchedbandedmatrix(dg, dq, q, element_bandwidth)
     return batchedbandedlu!(mat.data)
 end
+
+
+function Bennu.batchedbandedmatrix(
+        dg::SingleDirection,
+        q,
+        dq,
+        element_bandwidth=1;
+        time = 0
+    )
+    function matvec!(dq, q, event)
+        wait(event)
+        dg(dq, q, time; increment=false)
+        return Event(getdevice(dg))
+    end
+    mat = Bennu.batchedbandedmatrix(matvec!, dg.grid, dq, q, element_bandwidth)
+end
+
+function batchedbandedlu(
+        dg::SingleDirection,
+        q,
+        dq,
+        element_bandwidth=1;
+        time = 0
+    )
+    mat = Bennu.batchedbandedmatrix(dg, dq, q, element_bandwidth)
+    return batchedbandedlu!(mat.data)
+end
